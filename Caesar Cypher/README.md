68a43459353efbe873e6b5cbe218d20b465c9a33
unknown <karimiali0022@yahoo.com>
2020-03-05 23:46:12 +0800

    Caesar Cipher pwned

---
+++ Caesar Cypher/README.md
@@ -0,0 +1,13 @@
+## Caesar Cipher algorithm
+
+> Each letter in a given plane text is replaced by a letter with some fixed number (key) of positions.
+
+### Using the program
+
+Run the following command in your commandline:
+
+```shell
+python main.py [Function Code][Sentence][key value]
+```
+
+Note: Function code ==> 1 for encryption, 2 for decryption with a given key, and 3 for brute force decryption
---
+++ Caesar Cypher/main.py
@@ -0,0 +1,35 @@
+import sys
+
+# Encrypt plain text with given key
+def encrypt(text, key):
+    encryptedText = ""
+    for i in range (len(text)):
+        # based on values in ASCII 
+        decrement = 65 if text[i].isupper() else 97
+        encryptedText += chr((ord(text[i])-decrement + key)% 26 + decrement)
+    print (encryptedText)
+
+# Decrypt cipher text with given key
+def decrypt(cipher, key):
+    decryptedText = ""
+    for i in range(len(cipher)):
+        # based on values in ASCII 
+        decrement = 65 if cipher[i].isupper() else 97
+        decryptedText += chr((ord(cipher[i])-decrement - key)% 26 + decrement)
+    print('Decrypted with key ', key, ' ==> ', decryptedText)
+
+# Decrypt cipher text with brute force
+def decrypt_no_key(cipher):
+    for i in range(0,26):
+        decrypt(cipher, i)
+
+if __name__ == '__main__':
+    # to get the arguments passed to the program by the command line
+    args= sys.argv
+    if (len(args)< 3):
+        print("Not enough arguments")
+    else:
+        funcCode = int(args[1])
+        text = args[2]
+        key = int(args[3]) if len(args)>3 else 6
+        decrypt_no_key(text) if funcCode == 3 else (encrypt(text, key) if funcCode == 1 else decrypt(text, key))
