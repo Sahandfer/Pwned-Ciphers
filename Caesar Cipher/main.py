56cce8cbaca00750414538d6f6fdc69cff1c33f5
unknown <karimiali0022@yahoo.com>
2020-03-06 12:03:34 +0800

    prettified

--- Caesar Cipher/main.py
+++ Caesar Cipher/main.py
@@ -3,33 +3,39 @@ import sys
 # Encrypt plain text with given key
 def encrypt(text, key):
     encryptedText = ""
-    for i in range (len(text)):
-        # based on values in ASCII 
+    for i in range(len(text)):
+        # based on values in ASCII
         decrement = 65 if text[i].isupper() else 97
-        encryptedText += chr((ord(text[i])-decrement + key)% 26 + decrement)
-    print (encryptedText)
+        encryptedText += chr((ord(text[i]) - decrement + key) % 26 + decrement)
+    print(encryptedText)
+
 
 # Decrypt cipher text with given key
 def decrypt(cipherText, key):
     decryptedText = ""
     for i in range(len(cipherText)):
-        # based on values in ASCII 
+        # based on values in ASCII
         decrement = 65 if cipherText[i].isupper() else 97
-        decryptedText += chr((ord(cipherText[i])-decrement - key)% 26 + decrement)
-    print('Decrypted with key ', key, ' ==> ', decryptedText)
+        decryptedText += chr((ord(cipherText[i]) - decrement - key) % 26 + decrement)
+    print("Decrypted with key ", key, " ==> ", decryptedText)
+
 
 # Decrypt cipher text with brute force
 def decrypt_no_key(cipherText):
-    for key in range(0,26):
+    for key in range(0, 26):
         decrypt(cipherText, key)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # to get the arguments passed to the program by the command line
-    args= sys.argv
-    if (len(args)< 3):
+    args = sys.argv
+    if len(args) < 3:
         print("Not enough arguments")
     else:
         funcCode = int(args[1])
         text = args[2]
-        key = int(args[3]) if len(args)>3 else 6
-        decrypt_no_key(text) if funcCode == 3 else (encrypt(text, key) if funcCode == 1 else decrypt(text, key))
+        key = int(args[3]) if len(args) > 3 else 6
+        decrypt_no_key(text) if funcCode == 3 else (
+            encrypt(text, key) if funcCode == 1 else decrypt(text, key)
+        )
+
--- Vigenere Cipher/main.py
+++ Vigenere Cipher/main.py
@@ -7,9 +7,13 @@ def encrypt(text, key):
         keyIndex = i % len(key)
         textDecrement = 65 if text[i].isupper() else 97
         keyDecrement = 65 if key[keyIndex].isupper() else 97
-        encryptedText += chr((ord(text[i])+ord(key[keyIndex])-(textDecrement+keyDecrement) )% 26 + textDecrement)
+        encryptedText += chr(
+            (ord(text[i]) + ord(key[keyIndex]) - (textDecrement + keyDecrement)) % 26
+            + textDecrement
+        )
     print(encryptedText)
-        
+
+
 # Decrypt cipher text with given key
 def decrypt(cipherText, key):
     decryptedText = ""
@@ -17,20 +21,29 @@ def decrypt(cipherText, key):
         keyIndex = i % len(key)
         cipherDecrement = 65 if cipherText[i].isupper() else 97
         keyDecrement = 65 if key[keyIndex].isupper() else 97
-        decryptedText += chr((ord(cipherText[i])-ord(key[keyIndex])-(cipherDecrement-keyDecrement) )% 26 + cipherDecrement)
-    print('Decrypted with key ', key, ' ==> ', decryptedText)
+        decryptedText += chr(
+            (ord(cipherText[i]) - ord(key[keyIndex]) - (cipherDecrement - keyDecrement))
+            % 26
+            + cipherDecrement
+        )
+    print("Decrypted with key ", key, " ==> ", decryptedText)
+
 
 # Decrypt cipher text with brute force
 def decrypt_no_key(cipherText):
     print("Not enough computation power")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # to get the arguments passed to the program by the command line
-    args= sys.argv
-    if (len(args)< 3):
+    args = sys.argv
+    if len(args) < 3:
         print("Not enough arguments")
     else:
         funcCode = int(args[1])
         text = args[2]
-        key = int(args[3]) if len(args)>3 else 6
-        decrypt_no_key(text) if funcCode == 3 else (encrypt(text, key) if funcCode == 1 else decrypt(text, key))
+        key = int(args[3]) if len(args) > 3 else 6
+        decrypt_no_key(text) if funcCode == 3 else (
+            encrypt(text, key) if funcCode == 1 else decrypt(text, key)
+        )
+
