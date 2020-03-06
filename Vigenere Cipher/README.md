8f6da82d457403568fafd90f86de1239b24b3443
unknown <karimiali0022@yahoo.com>
2020-03-06 10:55:46 +0800

    Vigenere Cipher

---
+++ Vigenere Cipher/README.md
@@ -0,0 +1,13 @@
+## Vigenere Cipher algorithm
+
+> Each letter in a given plane text is shift by a letter with the int value of a given string.
+
+### Using the program
+
+Run the following command in your commandline:
+
+```shell
+python main.py [Function Code][Sentence][key value]
+```
+
+Note: Function code ==> 1 for encryption, 2 for decryption with a given key, and 3 for brute force decryption (Impossible)
---
+++ Vigenere Cipher/main.py
@@ -0,0 +1,38 @@
+import sys
+
+# Encrypt plain text with given key
+def encrypt(text, key):
+    encryptedText = ""
+    for i in range(len(text)):
+        keyIndex = i % len(key)
+        textDecrement = 65 if text[i].isupper() else 97
+        keyDecrement = 65 if key[keyIndex].isupper() else 97
+        encryptedText += chr((ord(text[i])+ord(key[keyIndex])-(textDecrement+keyDecrement) )% 26 + textDecrement)
+    print(encryptedText)
+        
+# Decrypt cipher text with given key
+def decrypt(cipherText, key):
+    decryptedText = ""
+    for i in range(len(cipherText)):
+        keyIndex = i % len(key)
+        cipherDecrement = 65 if cipherText[i].isupper() else 97
+        keyDecrement = 65 if key[keyIndex].isupper() else 97
+        decryptedText += chr((ord(cipherText[i])-ord(key[keyIndex])-(cipherDecrement-keyDecrement) )% 26 + cipherDecrement)
+    print('Decrypted with key ', key, ' ==> ', decryptedText)
+
+# Decrypt cipher text with brute force
+def decrypt_no_key(cipherText):
+    print("Not enough computation power")
+
+if __name__ == '__main__':
+    # to get the arguments passed to the program by the command line
+    args= sys.argv
+    encrypt("tellhimaboutme", "cafe")
+    decrypt("veqpjiredozxoe", "cafe")
+    if (len(args)< 3):
+        print("Not enough arguments")
+    else:
+        funcCode = int(args[1])
+        text = args[2]
+        key = int(args[3]) if len(args)>3 else 6
+        decrypt_no_key(text) if funcCode == 3 else (encrypt(text, key) if funcCode == 1 else decrypt(text, key))
