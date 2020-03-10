763d6873c1f0aada902536f806391c0e53f07c47
unknown <karimiali0022@yahoo.com>
2020-03-10 12:28:53 +0800

    One time pad

---
+++ One-time Pad/README.md
@@ -0,0 +1,13 @@
+## One time pad algorithm
+
+> Generate a random key with the same length as the key. The decrypted text is the bitwise or result of the key and the given plaintext.
+
+### Using the program
+
+Run the following command in your commandline:
+
+```shell
+python main.py [Function Code][Sentence][key value]
+```
+
+Note: Function code ==> 1 for encryption and 2 for decryption with a given key
---
+++ One-time Pad/main.py
@@ -0,0 +1,49 @@
+import sys
+import random
+import string
+
+
+def getKey(text):
+    letters = string.ascii_letters
+    return "".join(random.choice(letters) for i in range(len(text)))
+
+
+# Encrypt plain text with given key
+def encrypt(text):
+    encryptedText = ""
+    key = getKey(text)
+
+    for i in range(len(text)):
+        textDecrement = 65 if text[i].isupper() else 97
+        keyDecrement = 65 if key[i].isupper() else 97
+        encryptedText += chr(
+            (ord(text[i]) + ord(key[i]) - (textDecrement + keyDecrement)) % 26
+            + textDecrement
+        )
+
+    print("Encrypted with key ", key, " ==> ", encryptedText)
+
+
+# Decrypt cipher text with given key
+def decrypt(cipherText, key):
+    decryptedText = ""
+    for i in range(len(cipherText)):
+        cipherDecrement = 65 if cipherText[i].isupper() else 97
+        keyDecrement = 65 if key[i].isupper() else 97
+        decryptedText += chr(
+            (ord(cipherText[i]) - ord(key[i]) - (cipherDecrement - keyDecrement)) % 26
+            + cipherDecrement
+        )
+    print("Decrypted with key ", key, " ==> ", decryptedText)
+
+
+if __name__ == "__main__":
+    # to get the arguments passed to the program by the command line
+    args = sys.argv
+    if len(args) < 3:
+        print("Not enough arguments")
+    else:
+        funcCode = int(args[1])
+        text = args[2]
+        key = args[3] if len(args) > 3 else 6
+        encrypt(text) if funcCode == 1 else decrypt(text, key)
